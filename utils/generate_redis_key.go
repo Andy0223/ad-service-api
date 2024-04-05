ac467e1d3926adc5f39fee324148b21325a624d9
Andy0223 <andy.lee@bluextrade.com>
2024-04-05 18:54:55 +0800

    feat: Add function to generate Redis key from a map of strings

---
+++ utils/generate_redis_key.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"sort"
+)
+
+func GenerateRedisKey(key map[string]string) string {
+	redisKey := "ads"
+
+	// Create a slice of keys and sort it
+	keys := make([]string, 0, len(key))
+	for k := range key {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	// Iterate over keys in sorted order
+	for _, k := range keys {
+		redisKey += ":" + k + ":" + key[k]
+	}
+
+	return redisKey
+}
